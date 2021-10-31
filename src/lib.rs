2e24ead87681d4095e5156b98905fbdde52e84db
kamil <kamil@user.com>
2021-10-31 20:45:57 +0100

    moved tests to new folder

--- src/lib.rs
+++ src/lib.rs
@@ -41,12 +41,17 @@ pub mod graph {
 
         pub fn delate_versicles(&mut self, n1: usize, n2: usize) -> Result<(), &str> {
             if n1 < self.nodes.len() && n2 < self.nodes.len() {
-                self.versicles[n1][n2] = None;
-                Ok(())
+                match self.versicles[n1][n2] {
+                    None => Err("trying to delate non-existing verticle"),
+                    _ => {
+                        self.versicles[n1][n2] = None;
+                        Ok(())
+                    }
+                }
             } else {
                 Err("node number out of range")
             }
-        }
+        } 
 
         pub fn get_nodes_amount(&self) -> usize {
             self.nodes.len()
@@ -55,7 +60,7 @@ pub mod graph {
         pub fn get_versicles_amount(&self) -> usize {
             self.versicles_amount
         }
-
+        //TODO dodać tutaj mutowalne wartości 
         pub fn get_ver_value(&mut self, n1: usize, n2: usize) -> &Option<V> {
             if n1 < self.nodes.len() && n2 < self.nodes.len() {
                 &self.versicles[n1][n2]
@@ -79,56 +84,11 @@ pub mod graph {
                 versicles_amount: 0,
             }
         }
+
     }
 }
+
+
+
 #[cfg(test)]
-mod graph_test {
-    use crate::graph::Graph;
-
-    #[test]
-    fn adding_nodes_works() {
-        let mut g: Graph<i32, i32> = Graph::new();
-        assert_eq!(g.get_nodes_amount(), 0);
-
-        assert_eq!(g.add_node(1), 0);
-        assert_eq!(g.add_node(2), 1);
-        assert_eq!(g.add_node(3), 2);
-        assert_eq!(g.add_node(4), 3);
-
-        assert_eq!(g.get_nodes_amount(), 4);
-
-        assert_eq!(g.get_node_value(0), Some(&1));
-        assert_eq!(g.get_node_value(1), Some(&2));
-        assert_eq!(g.get_node_value(2), Some(&3));
-        assert_eq!(g.get_node_value(3), Some(&4));
-        assert_eq!(g.get_node_value(4), None);
-    }
-    #[test]
-    fn adding_vertices_works() {
-        let mut g: Graph<i32, i32> = Graph::new();
-        for i in 0..10 {
-            g.add_node(i);
-        }
-
-        assert_eq!(g.add_ver(0, 1, 1), Ok(()));
-        assert_eq!(g.add_ver(1, 2, 2), Ok(()));
-        assert_eq!(g.add_ver(2, 3, 4), Ok(()));
-        assert_eq!(g.add_ver(9, 1, 9), Ok(()));
-        assert_eq!(g.add_ver(4, 5, 1), Ok(()));
-        assert_eq!(g.add_ver(11, 2, 1), Err("node number out of range"));
-    }
-    #[test]
-    fn removing_nodes_works() {
-        let mut g: Graph<i32, i32> = Graph::new();
-        for i in 0..10 {
-            g.add_node(i);
-        }
-        assert_eq!(g.get_nodes_amount(),10);
-        assert_eq!(g.delate_node(0), Ok(()));
-        
-    }
-    #[test]
-    fn removing_ver_works() {
-
-    }
-}
+mod graph_tests ;
