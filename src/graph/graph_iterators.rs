d9b41ab2a0827efd5a2e1c58dd28759e663ea2f5
kamil <kamil@user.com>
2021-11-03 22:29:37 +0100

    add BFS tests

--- src/graph/graph_iterators.rs
+++ src/graph/graph_iterators.rs
@@ -1,23 +1,76 @@
+use std::collections::VecDeque;
 
-    use std::collections::VecDeque;
+use crate::graph::Graph;
+pub(crate) struct BFSIter<'a, V, N> {
+    graph: &'a Graph<V, N>,
+    queue: VecDeque<usize>,
+    unvisited: Vec<bool>,
+}
 
-    use crate::graph::Graph;
-    struct BFSIter<'a, V, N> {
-        graph: &'a Graph<V, N>,
-        current: usize,
-        queue: VecDeque<V>,
+impl<'a, V: Clone, N> BFSIter<'a, V, N> {
+    pub fn new(graph: &'a Graph<V, N>, start: usize) -> Self {
+        let mut new = Self {
+            graph,
+            queue: VecDeque::new(),
+            unvisited: vec![true; graph.get_nodes_amount()],
+        };
+        new.queue.push_back(start);
+        new
     }
+}
+// temporary unsafe
+impl<'a, V: Clone, N> Iterator for BFSIter<'a, V, N> {
+    type Item = &'a N;
 
-    impl<'a, V, N> BFSIter<'a, V, N> {
-        pub fn new(graph: &'a Graph<V, N>, start: usize) -> Self {
-            Self {
-                graph,
-                current : start,
-                queue : VecDeque::new(),
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.queue.pop_front() {
+            None => None,
+            Some(x) => {
+                self.unvisited[x] = false;
+                for n in self.graph.get_neighbors(x)? {
+                    if self.unvisited[n] {
+                        self.queue.push_back(n);
+                    }
+                }
+                Some(self.graph.get_node_value(x)?)
             }
         }
     }
+}
 
-    pub fn func(){}
+struct DFSIter<'a, V, N> {
+    graph: &'a Graph<V, N>,
+    stack: VecDeque<usize>,
+    unvisited: Vec<bool>,
+}
 
+impl<'a, V : Clone, N> DFSIter<'a, V, N> {
+    fn new(graph: &'a Graph<V, N>, start: usize) -> Self {
+        let mut new_one = Self {
+            graph,
+            stack : VecDeque::new(),
+            unvisited: vec![true; graph.get_nodes_amount()],
+        };
+        new_one.stack.push_back(start);
+        new_one
+    }
+}
 
+impl<'a, V: Clone, N> Iterator for DFSIter<'a, V, N> {
+    type Item = &'a N;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.stack.pop_back() {
+            None => None,
+            Some(x) => {
+                self.unvisited[x] = false;
+                for n in self.graph.get_neighbors(x)? {
+                    if self.unvisited[n] {
+                        self.stack.push_front(n);
+                    }
+                }
+                Some(self.graph.get_node_value(x)?)
+            }
+        }
+    }
+}
